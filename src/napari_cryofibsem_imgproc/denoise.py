86fdb5f9e1b7ec6ec884cc78a1f399d484c2a9ae
Shaina To <165912537+shainato@users.noreply.github.com>
2025-01-17 11:53:44 +0100

    Update denoise.py
    Added choices for gradient threshold

--- src/napari_cryofibsem_imgproc/denoise.py
+++ src/napari_cryofibsem_imgproc/denoise.py
@@ -65,7 +65,8 @@ def process_slice(slice_data, iteration, b):
     image={"label": "Input Image"},
     iteration={"label": "Iteration"},
     b={"label": "Gradient Threshold",
-       "choices": ["0.01", "0.02", "0.03", "0.04", "0.05", "0.06", "0.07", "0.08", "0.09", "0.10"]}
+       "choices": ["0.01", "0.02", "0.03", "0.04", "0.05", "0.06", "0.07", "0.08", "0.09", "0.10",
+                  "0.11", "0.12", "0.13", "0.14", "0.15", "0.16", "0.17", "0.18", "0.19", "0.20"]}
 )
 def denoise(
         image: Image,
